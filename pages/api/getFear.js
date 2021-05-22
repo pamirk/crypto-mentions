f7aa4ee045d43a4fd551365cd84ac459bdf255e2
pamir <pmysteryk@gmail.com>
2021-05-22 10:05:02 +0500

    simple test v5

--- pages/api/getFear.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const {GoogleSpreadsheet} = require('google-spreadsheet');
-const fetch = require("isomorphic-fetch");
-const url = 'https://api.alternative.me/fng/?limit=31';
-export default async (req, res) => {
-    const _res = await fetch(url).then(res => res.json())
-    return res.status(200).json(_res)
-}
