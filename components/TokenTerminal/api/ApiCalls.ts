056f7452c74229b42c771ecb8d42d90bec4a7e22
Pamir khan <31883250+pamirk@users.noreply.github.com>
2021-06-21 15:47:35 +0500

    Update ApiCalls.ts

--- components/TokenTerminal/api/ApiCalls.ts
+++ components/TokenTerminal/api/ApiCalls.ts
@@ -24,7 +24,7 @@ let d = new Date();
 // let seconds = Math.floor(d.getTime() / 1000);
 //1623060669 1623773241
 //1623912926
-const dataHash = 1624037083
+const dataHash = 1624267053
 const postFix = `data_hash=${dataHash}`
 const prefix = "https://api.tokenterminal.com"
 
